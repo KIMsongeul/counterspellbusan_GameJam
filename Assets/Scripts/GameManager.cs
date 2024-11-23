fd9de8cc9b1aa002a698ac6343642d07a5395486
gunggme <87892724+gunggme@users.noreply.github.com>
2024-11-23 18:18:24 +0900

    플레이어 소환 스크립트는 완성
    플레이어는 아직 안움직임

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,29 +9,23 @@ public class GameManager : MonoBehaviourPunCallbacks
     public GameObject playerPrefab; // 플레이어 프리팹
     public Transform[] spawnPoints; // 플레이어 스폰 위치
 
-    private void Start()
-    {
-        // 포톤 서버 연결
-        PhotonNetwork.ConnectUsingSettings();
-    }
+    void Start(){
+        // 플레이어 생성 및 색상 설정
+        GameObject player;
+        if(PhotonNetwork.IsMasterClient)
+        {
+            player = PhotonNetwork.Instantiate($"Prefabs/{playerPrefab.name}", spawnPoints[0].position, Quaternion.identity);
+        }
+        else
+        {
+            player = PhotonNetwork.Instantiate($"Prefabs/{playerPrefab.name}", spawnPoints[1].position, Quaternion.identity);
+        }
 
-    public override void OnConnectedToMaster()
-    {
-        Debug.Log("마스터 서버에 연결되었습니다.");
-        // 로비 입장
-        PhotonNetwork.JoinLobby();
-    }   
-
-    // 룸 입장 성공시 호출
-    public override void OnJoinedRoom()
-    {
-        Debug.Log("룸에 입장하였습니다.");
-        
-        // 랜덤한 스폰 포인트 선택
-        int randomSpawnPoint = Random.Range(0, spawnPoints.Length);
-        Vector3 spawnPosition = spawnPoints[randomSpawnPoint].position;
-        
-        // 플레이어 생성
-        PhotonNetwork.Instantiate(playerPrefab.name, spawnPosition, Quaternion.identity);
+        // 플레이어 색상 설정
+        SpriteRenderer sr = player.GetComponentInChildren<SpriteRenderer>();
+        if(player.GetComponent<PhotonView>().IsMine)
+        {
+            sr.color = Color.blue; // 자신은 파란색
+        }
     }
 }
