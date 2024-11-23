4b6076073901e51c6cd0bb241c46ad06c7ea912c
gunggme <87892724+gunggme@users.noreply.github.com>
2024-11-24 04:05:59 +0900

    아이템 버튼

--- Assets/Scripts/ItemButton.cs
+++ Assets/Scripts/ItemButton.cs
@@ -1,10 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using Photon.Pun;
+using UnityEngine.UI;
 
-public class ItemButton : MonoBehaviour
+public class ItemButton : MonoBehaviour, IPointerDownHandler
 {
     public void OnClick(){
         print("ItemButton OnClick");
+    }
+
+    public void OnPointerDown(PointerEventData eventData){
+        print("ItemButton OnPointerDown");
+        
     }
 }
