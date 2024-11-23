102055c28e0ac4f7e38a333a22faaf2ca14dc4b1
gunggme <87892724+gunggme@users.noreply.github.com>
2024-11-23 15:47:11 +0900

    포톤 패키지 추가

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    private void Start()
+    {
+        // 포톤 서버 연결
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // 마스터 서버 연결 성공시 호출
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("마스터 서버에 연결되었습니다.");
+        // 로비 입장
+        PhotonNetwork.JoinLobby();
+    }   
+    // 룸 입장 성공시 호출
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("룸에 입장하였습니다.");
+    }
+}
